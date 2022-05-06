9882d6e2b31ff544a5ed3cd9553f2862f0532080
Roldine Davilmar <rdavilmar@ets.org>
2022-05-06 15:32:16 -0400

    updated input boxes

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,17 @@ var date = currentDate.getDate();
 var day = daysofWeek[currentDate.getDay()];
 
 var current = day + ", " + (month<10 ? '0' : '') + month + '/' + (date<10 ? '0' : '') + date + '/' + currentDate.getFullYear();
+var hourSlots = ["9am", "10am", "11am", "noon", "1pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm"]
 
 $(document).ready(function () {
 
 document.getElementById('currentDay').innerHTML = current;
 
+for (let i = 0; i < 14; i++) {
+    var timeSlots = $('<h3 class="hour">' + hourSlots[i] + '</h3>');
+        timeSlots.appendTo('.container')
+    var timeBlocks = $('<input type="text" name="task" class="time-block" value=""/>');
+        timeBlocks.appendTo('.container');
+}
 
 });
